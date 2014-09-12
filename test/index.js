06b16cee710eec9422de3863569b7516eea581a8
camshaft <bytheway.cameron@gmail.com>
2014-09-11 21:45:48 -0600

    initial commit

---
+++ test/index.js
@@ -0,0 +1,6 @@
+describe('package-hyper', function() {
+  require('directiv-data-hyper/test');
+  require('directiv-data-hyper-bind/test');
+  require('directiv-data-hyper-repeat/test');
+  require('directiv-data-hyper-img/test');
+});
